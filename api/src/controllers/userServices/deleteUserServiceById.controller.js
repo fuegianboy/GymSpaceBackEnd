cda3d5aa276067b6ff4df86983a46a87897f43e2
fuegianboy <fuegianboy@hotmail.com>
2023-11-16 16:58:31 -0300

    add-UserProducts-UserServices-controllers

---
+++ api/src/controllers/userServices/deleteUserServiceById.controller.js
@@ -0,0 +1,20 @@
+const { UserServices } = require("../../db")
+
+const deleteUserServiceById = async (req, res) => {
+
+    let {id} = req.params
+    const userServiceID = await UserServices.findByPk(id)
+
+    if(!userServiceID) {
+        return res.status(400).send({message:"Service not found"})
+    }
+
+    try {
+        await userServiceID.destroy()
+        return res.status(200).json("Service deleted")
+    } catch (error) {
+        return res.status(500).send({ message: "Error deleting UserService record", error: error.message });
+      }
+}
+
+module.exports = deleteUserServiceById;

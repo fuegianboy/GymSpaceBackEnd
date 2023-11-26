cd345d1475fe2b04d3a57d29d2bfb7e56271c5f5
Kevin_Padilla <kevin0696s@gmail.com>
2023-11-26 18:55:03 -0400

    All routes were protected

--- api/src/controllers/coaches/deleteCoachById.controller.js
+++ api/src/controllers/coaches/deleteCoachById.controller.js
@@ -1,11 +1,18 @@
 const { Coaches } = require("../../db");
 const { isValidUUID } = require("../../utils");
-
+const {getUUID, isAuthorized} = require("../../utils/AuthUtils")
 const deleteCoachById = async (req, res) => {
 
     try {
         const { id } = req.params;
 
+        const auth0User = await req.auth.payload.sub
+        const rolesAllowed = ["Admin"]
+        const userUUID = await getUUID(auth0User)
+        
+        if(!await isAuthorized(userUUID,rolesAllowed)){
+            return res.status(403).json({error: `only ${rolesAllowed} allowed`})
+        }
         if (!isValidUUID(id))
             return res.status(404).json({ error: "id must be UUID format" })
 
--- api/src/controllers/coaches/updateCoach.controller.js
+++ api/src/controllers/coaches/updateCoach.controller.js
@@ -1,11 +1,33 @@
 const { Coaches } = require("../../db");
-
+const { getUUID, isAuthorized } = require("../../utils/AuthUtils");
+const { isValidUUID } = require("../../utils")
 const updateCoach = async (req, res) => {
 
     try {
         const data = req.body;
         const {id} = req.params;
 
+        const userUUID = isValidUUID(id) ? id : getUUID(id);
+        const auth0User = await req.auth.payload.sub;
+        const rolesAllowed = ["Admin"];
+        const auth0UserUUID = await getUUID(auth0User);
+        
+        if (await isAuthorized(auth0UserUUID, rolesAllowed) || auth0UserUUID === userUUID) {
+            
+          } else {
+            return res.status(403).json({ error: "Only the user and administrator are allowed to edit the user" });
+          }
+        // An User is not allowed to change his status or role
+        if (auth0UserUUID === userUUID) {
+          const { systemRole, status } = data;
+          if (systemRole || status) {
+            return res
+              .status(403)
+              .json({
+                error: `The user does not have permissions to change their role or status`,
+              });
+          }
+        }
         const coach = await Coaches.findByPk(id);
 
         if(!coach) {

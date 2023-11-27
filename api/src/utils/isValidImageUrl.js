918d704af08d19ed5d1204ce6f78f60cc04e1caa
Luis Mesajil <lhmesajil@gmail.com>
2023-11-26 19:02:41 -0500

    Fix url validator

--- api/src/utils/isValidImageUrl.js
+++ api/src/utils/isValidImageUrl.js
@@ -3,9 +3,10 @@
  */
 const isValidImageUrl = (url) => {
     // Regular expression to match common image file extensions
-    const imageFormatRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
+    // const imageFormatRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
     // Regular expression to match a URL pattern
     const urlRegex = /^(http(s)?:\/\/)?[^\s\/$.?#].[^\s]*$/i;
+    return urlRegex.test(url)
     return urlRegex.test(url) && imageFormatRegex.test(url);
 }
 

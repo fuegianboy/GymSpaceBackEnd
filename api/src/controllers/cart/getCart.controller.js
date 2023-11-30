4e7c70c1fcb2b09e82bff15fb68989f5985199b3
Kevin_Padilla <kevin0696s@gmail.com>
2023-11-30 10:04:56 -0400

    Adding services to the cart

--- api/src/controllers/cart/getCart.controller.js
+++ api/src/controllers/cart/getCart.controller.js
@@ -1,27 +1,54 @@
 const { Op } = require("sequelize");
-const { Products, UserProducts } = require("../../db");
+const { Products, UserProducts, UserServices, Services } = require("../../db");
 const { getUUID } = require("../../utils/AuthUtils");
 const getCart = async (req, res) => {
   try {
     const { id } = req.params;
     const userUUID = await getUUID(id);
-    const userCart = await UserProducts.findAll({
+    const productsCart = await UserProducts.findAll({
+      where: {
+        userID: userUUID,
+        mp_status: "inCart",
+      },
+    });
+    const servicesCart = await UserServices.findAll({
       where: {
         userID: userUUID,
         mp_status: "inCart",
       },
     });
 
-    const userCartResponse = [];
+    const productsCartResponse = [];
 
-    for (const item of userCart) {
+    for (const item of productsCart) {
       let product = await Products.findByPk(item.productID);
+      if(product){
       product = product.toJSON();
       const transformedProduct = { ...product, quantity: item.qty };
-      userCartResponse.push(transformedProduct);
+      productsCartResponse.push(transformedProduct);
     }
-    console.log(userCartResponse);
-    return res.status(200).json(userCartResponse);
+    }
+
+    const servicesCartResponse = [];
+
+    
+    for (const item of servicesCart) {
+      let service = await Services.findByPk(item.serviceID);
+      if(service){
+      service = service.toJSON();
+      const transformedService = {
+        ...service,
+        quantity: item.qty,
+        startDate: item.startDate,
+      };
+      servicesCartResponse.push(transformedService);
+      
+    }
+    }
+    
+    return res
+      .status(200)
+      .json({ products: productsCartResponse, services: servicesCartResponse });
   } catch (error) {
     console.error(error);
     return res

b00d78ec7eb247c36e4d639d1537f9f31069c732
fuegianboy <fuegianboy@hotmail.com>
2023-12-01 16:46:36 -0300

    addDeleteControllers/ModifyphoneRules

--- api/src/routes/userProducts.js
+++ api/src/routes/userProducts.js
@@ -4,6 +4,7 @@ const getUserProductById = require("../controllers/userProducts/getUserProductBy
 const createUserProduct = require("../controllers/userProducts/createUserProducts.controller");
 const updateUserProduct = require("../controllers/userProducts/updateUserProducts.controller");
 const deleteUserProductById = require("../controllers/userProducts/deleteUserProductsById.controller");
+const deleteUserProductsByUserId = require("../controllers/userProducts/deleteUserProductByUserID.controller");
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.get("/:id", getUserProductById)
 router.post("/", createUserProduct)
 router.put("/:id", updateUserProduct)
 router.delete("/:id", deleteUserProductById)
+router.delete("/", deleteUserProductsByUserId)
 
 
 module.exports = router
